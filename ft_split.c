4bac2b3e6e857ca2384877805dc6fd095722e14c
Fran Marín Pérez <franmp97@outlook.com>
2023-10-04 13:54:06 +0200

    new split -- gnl commented\!!

--- ft_split.c
+++ ft_split.c
@@ -3,95 +3,88 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fmarin-p <fmarin-p@student.42madrid>       +#+  +:+       +#+        */
+/*   By: fmarin-p <fmarin-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/28 15:05:18 by fmarin-p          #+#    #+#             */
-/*   Updated: 2022/03/30 20:53:50 by fmarin-p         ###   ########.fr       */
+/*   Created: 2023/10/03 22:43:55 by fmarin-p          #+#    #+#             */
+/*   Updated: 2023/10/04 13:52:20 by fmarin-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	**ft_malloc_error(char **p)
-{
-	unsigned int	i;
-
-	i = 0;
-	while (p[i])
-	{
-		free(p[i]);
-		i++;
-	}
-	free(p);
-	return (0);
-}
-
-static char	**ft_allocdbpt(char const *s, char c)
+size_t	count_delimiter(char const *str, int delimiter)
 {
 	int		i;
-	int		count;
-	char	**p;
+	size_t	count;
 
+	i = 0;
 	count = 0;
-	while (*s)
+	while (str[i])
 	{
-		i = 0;
-		while (*s != c && *s)
+		if (str[i] != delimiter)
 		{
-			++i;
-			++s;
+			while (str[i] != delimiter && str[i])
+				i++;
+			count++;
 		}
-		if (i)
-			++count;
-		if (*s)
-			++s;
+		if (!str[i])
+			break ;
+		i++;
 	}
-	p = (char **) malloc(sizeof(char *) * (count + 1));
-	if (!p)
-		return (0);
-	p[count] = 0;
-	return (p);
+	return (count);
 }
 
-static int	ft_allocpt(char const *s, char **p, int i, int i2)
+size_t	count_str(const char *str, int i, int delimiter)
 {
-	int	i3;
+	int	size;
 
-	p[i2] = (char *) malloc(sizeof(char) * (i + 1));
-	if (!p[i2])
-		return (0);
-	p[i2][i] = 0;
-	i3 = 0;
-	while (i)
-		p[i2][i3++] = *(s - i--);
-	return (1);
+	size = 0;
+	while (str[i] != delimiter && str[i])
+	{
+		size++;
+		i++;
+	}
+	return (size);
+}
+
+char	*allocate_substring(const char *str, int *i, int delimiter)
+{
+	char	*substr;
+	size_t	size;
+	int		index;
+
+	size = count_str(str, *i, delimiter) + 1;
+	substr = ft_calloc(size, sizeof(char));
+	index = 0;
+	while (--size)
+		substr[index++] = str[(*i)++];
+	return (substr);
+}
+
+int	allocate_strings(char **split, char const *s, char c)
+{
+	int		i;
+	int		index;
+
+	i = -1;
+	index = 0;
+	while (s[++i])
+	{
+		while (s[i] != c && s[i])
+			split[index++] = allocate_substring(s, &i, c);
+		if (!s[i])
+			break ;
+	}
+	return (0);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	**p;
-	int		i;
-	int		i2;
+	char	**split;
 
-	if (!s)
-		return (0);
-	p = ft_allocdbpt(s, c);
-	if (!p)
-		return (0);
-	i2 = 0;
-	while (*s)
-	{
-		i = 0;
-		while (*s != c && *s)
-		{
-			++i;
-			++s;
-		}
-		if (i)
-			if (!ft_allocpt(s, p, i, i2++))
-				return (ft_malloc_error(p));
-		if (*s)
-			++s;
-	}
-	return (p);
+	split = ft_calloc(count_delimiter(s, c) + 1, sizeof(char *));
+	if (!split)
+		return (NULL);
+	allocate_strings(split, s, c);
+	return (split);
 }
