93d571549a9839716be7c0e06879ce24d1a88027
Fran Marín Pérez <franmp97@outlook.com>
2023-10-15 01:03:34 +0200

    realloc prototype changed

--- ft_realloc.c
+++ ft_realloc.c
@@ -6,20 +6,21 @@
 /*   By: fmarin-p <fmarin-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 23:22:20 by fmarin-p          #+#    #+#             */
-/*   Updated: 2023/10/15 00:01:03 by fmarin-p         ###   ########.fr       */
+/*   Updated: 2023/10/15 01:02:51 by fmarin-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_realloc(void *p, size_t old_size, size_t new_size)
+void	*ft_realloc(void *p, size_t old_count,
+		size_t new_count, size_t type_size)
 {
 	void	*new_p;
 
-	new_p = ft_calloc(1, new_size);
+	new_p = ft_calloc(1, new_count * type_size);
 	if (!new_p)
 		return (NULL);
-	ft_memmove(new_p, p, old_size);
+	ft_memmove(new_p, p, old_count * type_size);
 	free(p);
 	return (new_p);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fmarin-p <fmarin-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 15:50:08 by fmarin-p          #+#    #+#             */
-/*   Updated: 2023/10/15 00:01:13 by fmarin-p         ###   ########.fr       */
+/*   Updated: 2023/10/15 01:03:13 by fmarin-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,8 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
-void	*ft_realloc(void *p, size_t old_size, size_t new_size);
+void	*ft_realloc(void *p, size_t old_count,
+			size_t new_count, size_t type_size);
 char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
